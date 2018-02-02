33288940d7bd2267881a8fd74f46f0bcf1763a7d
Indychick <kimmylyndon@gmail.com>
2018-02-02 16:48:12 -0500

    Initial Commit

---
+++ SinglePhoto/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SinglePhoto
+//
+//  Created by Kim Lyndon on 2/2/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
